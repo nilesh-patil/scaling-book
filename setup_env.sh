28c602ea8b3419d73b02c4a6a73568bb544b015c
Nilesh <4813079+nilesh-patil@users.noreply.github.com>
2025-09-14 19:53:54 +0530

    Comment out Ruby gem installation in setup_env.sh and update jekyll.yml to source .bashrc before building with Jekyll for improved environment consistency.

--- setup_env.sh
+++ setup_env.sh
@@ -4,6 +4,6 @@ pixi install
 
 # Setup Ruby environment
 
-pixi run gem install csv bundler:2.7.2
+#pixi run gem install csv bundler:2.7.2
 pixi run bundle install
 pixi run bundle exec jekyll build
